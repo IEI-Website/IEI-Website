63fe017643698a113b19dd7113b808e3d068f26a
kaliappan <skaliappan1908@gmail.com>
2022-04-16 12:23:52 +0530

    Feature: Backend pdf merge
    merging the user document and response on the backend on api call

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var multer = require('multer')
 var cors = require('cors')
+var fs = require('fs');
 var app = express();
+const merge = require('easy-pdf-merge');
 
+var responseNo = 1;
 app.options('*', cors())
 
 app.use((req, res, next)=>{
@@ -12,20 +15,23 @@ app.use((req, res, next)=>{
 });
 
 app.post('/upload', (req, res) => {
-  console.log("request body : ",res.data);
+  console.log("request body : ",req.files);
   const storage = multer.diskStorage({
+    
     destination: function (req, file, cb) {
-      cb(null, "/home/kaliappan/Documents/work/college/IEI/IEI-Website/backend/docs/")
+      const dirName = "/home/kaliappan/Documents/work/college/IEI/IEI-Website/backend/docs/"+Date.now() + '-' + Math.round(Math.random() * 1E9)
+    fs.mkdirSync(dirName);
+    console.log(dirName);
+      cb(null,dirName);
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.originalname)
+      cb(null, file.originalname);
     }
   })
   
-  const upload = multer({ storage: storage }).single('userFile')
+  const upload = multer({ storage: storage }).any();
   upload(req, res, function (err) {
-    console.log("request file : ");
+    console.log("request file : ", req.files);
     if (err instanceof multer.MulterError) {
       console.log("Err ",err)
       return
@@ -38,6 +44,67 @@ app.post('/upload', (req, res) => {
     // Everything went fine.
   })
 })
+
+
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + file.originalname
+    );
+  },
+});
+
+var dir = "public";
+var subDirectory = "public/uploads/";
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+
+  fs.mkdirSync(subDirectory);
+}
+
+var mergepdffilesupload = multer({storage:storage})
+
+
+app.post('/mergepdf',mergepdffilesupload.array('userDoc',2),(req,res) => {
+  const files = []
+  outputFilePath = dir+"/response/response_"+responseNo+".pdf";
+  responseNo++;
+  if(req.files){
+    req.files.forEach(file => {
+      console.log(file.path)
+      var filePath = file.path;
+      console.log(file.path)
+      if (!filePath.endsWith(".pdf")){
+        fs.rename(filePath,filePath+".pdf", () => {
+          console.log("\nFile Renamed!\n");
+        });        
+        filePath = filePath+".pdf";
+      }
+      files.push(filePath)
+    });
+
+      merge(files,outputFilePath, function (err) {
+          files.forEach(file => {
+        fs.unlink(file, function (err) {
+          if (err) throw err;
+          // if no error, file has been deleted successfully
+          console.log('File deleted!');
+        });
+        });
+          if (err) {
+              return console.log(err)
+          }
+          console.log('Success')
+      });
+}
+  });
 
 var server = app.listen(3000, ()=>{
     console.log("port used", server.address().port);
